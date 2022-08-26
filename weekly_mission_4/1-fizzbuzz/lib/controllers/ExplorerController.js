cdda879f55766f3dc13ad26b48152160c08c7727
EdgarVRP <edgarvrp22@gmail.com>
2022-08-26 08:32:59 -0500

    Create ExplorerController.js

---
+++ weekly_mission_4/1-fizzbuzz/lib/controllers/ExplorerController.js
@@ -0,0 +1,8 @@
+const ExplorerService = require("../services/ExplorerService");
+const Reader = require("../utils/Reader");
+const FizzbuzzService = require("../services/FizzbuzzService");
+
+
+
+
+module.exports = ExplorerController;
